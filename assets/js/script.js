21ea051125223b513818c6275b0f9089df10d26f
brownradical <79535503+brownradical@users.noreply.github.com>
2021-04-25 22:17:16 -0500

    fix indentation formatting in js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,80 +1,75 @@
 $(function () {});
 
-  var today = moment().format("dddd, MMMM Do");
-  var hour = moment().format("HA");
+var today = moment().format("dddd, MMMM Do");
+var hour = moment().format("HA");
 
-  $("#currentDay").text(today);
-
-
-  /* HOURS OF THE DAY*/
-  var planDay = [
-    { time: "9AM", action: "" },
-    { time: "10AM", action: "" },
-    { time: "11AM", action: "" },
-    { time: "12PM", action: "" },
-    { time: "1PM", action: "" },
-    { time: "2PM", action: "" },
-    { time: "3PM", action: "" },
-    { time: "4PM", action: "" },
-    { time: "5PM", action: "" },
-  ];
+$("#currentDay").text(today);
 
 
-  /* ROW CREATION */
-  planDay.forEach(function(timeBlock,index) {
-
-    var timeLabel = timeBlock.time;
-    var blockColor = colorRow(timeLabel);
-    var row =
-      '<div class="time-block" id="' +
-      index +
-      '"><div class="row no-gutters input-group"><div class="col-sm col-lg-1 input-group-prepend hour justify-content-sm-end pr-3 pt-3">' +
-      timeLabel +
-      '</div><textarea class="form-control ' +
-      blockColor +
-      '">' +
-		  timeBlock.action +
-		  '</textarea><div class="col-sm col-lg-1 input-group-append"><button class="saveBtn btn-block" type="submit"><i class="fas fa-save"></i></button></div></div></div>';
-
-      $(".container").append(row);
-  });
+/* HOURS OF THE DAY*/
+var planDay = [
+  { time: "9AM", action: "" },
+  { time: "10AM", action: "" },
+  { time: "11AM", action: "" },
+  { time: "12PM", action: "" },
+  { time: "1PM", action: "" },
+  { time: "2PM", action: "" },
+  { time: "3PM", action: "" },
+  { time: "4PM", action: "" },
+  { time: "5PM", action: "" },
+];
 
 
-  /* ROW COLORS */
-  function colorRow(time) {
+/* ROW CREATION */
+planDay.forEach(function(timeBlock,index) {
 
-    var planHour = moment(hour, "HA");
-	  var planEntry = moment(time, "HA");
-	  if (planHour.isBefore(planEntry) === true) {
-		  return "future";
-	  } else if (planHour.isAfter(planEntry) === true) {
-		  return "past";
-	  } else {
-		  return "present";
-	  }
-  }
+  var timeLabel = timeBlock.time;
+  var blockColor = colorRow(timeLabel);
+  var row =
+    '<div class="time-block" id="' +
+    index +
+    '"><div class="row no-gutters input-group"><div class="col-sm col-lg-1 input-group-prepend hour justify-content-sm-end pr-3 pt-3">' +
+    timeLabel +
+    '</div><textarea class="form-control ' +
+    blockColor +
+    '">' +
+		timeBlock.action +
+		'</textarea><div class="col-sm col-lg-1 input-group-append"><button class="saveBtn btn-block" type="submit"><i class="fas fa-save"></i></button></div></div></div>';
+
+    $(".container").append(row);
+});
 
 
-  /* SAVING + STORAGE */
-  $(".saveBtn").on("click", function() {
-      var blockID = parseInt(
-        $(this)
-          .closest(".time-block")
-          .attr("id")
-      );
-      var userEntry = $.trim(
-        $(this)
-          .parent()
-          .siblings("textarea")
-          .val()
-      );
+/* ROW COLORS */
+function colorRow(time) {
 
-      planDay[blockID].action = userEntry;
+  var planHour = moment(hour, "HA");
+	var planEntry = moment(time, "HA");
+	if (planHour.isBefore(planEntry) === true) {
+		return "future";
+	} else if (planHour.isAfter(planEntry) === true) {
+		return "past";
+	} else {
+		return "present";
+	}
+}
+
+
+/* SAVING + STORAGE */
+$(".saveBtn").on("click", function() {
+    var blockID = parseInt(
+      $(this)
+        .closest(".time-block")
+        .attr("id")
+    );
+    var userEntry = $.trim(
+      $(this)
+        .parent()
+        .siblings("textarea")
+        .val()
+     );
+
+    planDay[blockID].action = userEntry;
       
-      localStorage.setItem("Day", JSON.stringify(planDay));
-  });
-
-    var workActions = JSON.parse(localStorage.getItem("Day"));
-    if (workActions) {
-      planDay = workActions;
-   }
+     localStorage.setItem("Day", JSON.stringify(planDay));
+});
